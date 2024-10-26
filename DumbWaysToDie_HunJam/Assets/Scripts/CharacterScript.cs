ee0c0e867c311b4fc3fad5d9fdbc346c93966d2b
sztLevente <levente.szt@gmail.com>
2024-10-26 15:04:39 +0200

    Projekt vaz

---
+++ DumbWaysToDie_HunJam/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,40 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    private Vector2 RightSpeed = new Vector2(5, 0);
+    private Vector2 LeftSpeed = new Vector2(-5, 0);
+
+    public Rigidbody2D CharacterRigidbody2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CharacterRigidbody2D.linearVelocity = RightSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            CharacterRigidbody2D.linearVelocity = new Vector2(0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CharacterRigidbody2D.linearVelocity = LeftSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            CharacterRigidbody2D.linearVelocity = new Vector2(0,0);
+        }
+    }
+    public void Die()
+    {
+        
+    }
+}

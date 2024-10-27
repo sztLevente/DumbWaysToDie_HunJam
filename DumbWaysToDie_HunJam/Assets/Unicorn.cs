56207f9d78167d958d51b94d686ca0baa4533625
Boti2003 <natkay.botond@gmail.com>
2024-10-27 17:00:19 +0100

    geciiii

---
+++ DumbWaysToDie_HunJam/Assets/Unicorn.cs
@@ -0,0 +1,53 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class Unicorn : MonoBehaviour
+{
+
+
+    public Sprite ActionSprite;
+    public Sprite SleepSprite;
+    public AnimatorController Action;
+    public AnimatorController SleepAnimation;
+    private float timeSinceStart = 0;
+    public Animator animator;
+    private bool killer = false;
+    public GameObjectControllerScript gameObjectController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Character" && killer)
+        {
+            gameObjectController.KillCharacter();
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = ActionSprite;
+        animator.runtimeAnimatorController = Action;
+        killer = true;
+        
+       
+    }
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        if (timeSinceStart < 1.0f)
+        {
+            GetComponent<Rigidbody2D>().linearVelocity = new Vector3(2, 0, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
+        }
+        if (timeSinceStart >= 10.0f)
+        {
+            GetComponent<SpriteRenderer>().sprite = SleepSprite;
+            animator.runtimeAnimatorController = SleepAnimation;
+            killer = false;
+        }
+
+    }
+}
